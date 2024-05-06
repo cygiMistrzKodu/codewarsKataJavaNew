206e657ff1a0eeb47edbafe9c11d9a1f72a66026
Jacek programmer <jacek301@gmail.com>
2024-05-06 21:15:25 +0200

    How good are you really Kata Solution

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+
 public class Kata {
     public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
-        // Your code here
-        return true;
+
+        int sumClassPoints = Arrays.stream(classPoints).sum();
+        int countClassPoints = (int) Arrays.stream(classPoints).count();
+
+        int allClassPoints = sumClassPoints + yourPoints;
+        int countAll = countClassPoints + 1;
+
+        int avgPoints =  allClassPoints / countAll;
+
+        return avgPoints < yourPoints;
     }
 }
