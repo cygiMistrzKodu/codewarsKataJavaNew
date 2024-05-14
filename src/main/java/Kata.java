ae206db0523e7feca5cbcbfb54f8ac94ea00650b
Jacek programmer <jacek301@gmail.com>
2024-05-15 01:52:29 +0200

    Beginner - Reduce but Grow Kata Start

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,17 +1,9 @@
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Kata {
-    public static String reverseWords(final String original) {
 
-        if (original.isBlank()) {
-            return original;
-        }
+    public static int grow(int[] x) {
 
-        List<String> splitByWorldSpaceIncluded = List.of(original.split(" "));
-
-        return splitByWorldSpaceIncluded.stream().map(word -> new StringBuilder(word).reverse())
-                .collect(Collectors.joining(" "));
+        return 0;
 
     }
+
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,34 +1,15 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
+import java.util.*;
 
+// TODO: Replace examples and use TDD development by writing your own tests
 
 public class SolutionTest {
     @Test
-    public void exampleCases() {
-        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", Kata.reverseWords("The quick brown fox jumps over the lazy dog."));
-        assertEquals("elppa", Kata.reverseWords("apple"));
-        assertEquals("a b c d", Kata.reverseWords("a b c d"));
-        assertEquals("elbuod  decaps  sdrow", Kata.reverseWords("double  spaced  words"));
-    }
-
-    @Test
-    public void doubleSpaced() {
-        assertEquals("elbuod  decaps  sdrow", Kata.reverseWords("double  spaced  words"));
-    }
-
-    @Test
-    public void reverseOneWord() {
-        assertEquals("elppa", Kata.reverseWords("apple"));
-    }
-
-    @Test
-    public void singleLetterReverse() {
-        assertEquals("a b c d", Kata.reverseWords("a b c d"));
-    }
-
-    @Test
-    public void allSpace() {
-        assertEquals("    ", Kata.reverseWords("    "));
+    public void testSomething() {
+        assertEquals(6, Kata.grow(new int[]{1,2,3}));
+        assertEquals(16, Kata.grow(new int[]{4,1,1,1,4}));
+        assertEquals(64, Kata.grow(new int[]{2,2,2,2,2,2}));
     }
 }
