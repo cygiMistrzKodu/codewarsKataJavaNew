55cdc680a5c84e253c699de5fc15fbd40676e2f7
Jacek programmer <jacek301@gmail.com>
2024-05-15 02:17:45 +0200

    Square(n) Sum Kata start

---
+++ src/main/java/Kata.java
@@ -0,0 +1,8 @@
+public class Kata
+{
+    public static int squareSum(int[] n)
+    {
+        //Your Code
+        return 0;
+    }
+}
--- src/main/java/Maps.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-public class Maps {
-    public static int[] map(int[] arr) {
-
-        return Arrays.stream(arr).map(number -> number * 2).toArray();
-    }
-}
