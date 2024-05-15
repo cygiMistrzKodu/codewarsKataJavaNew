9dda3c6d8cba302a4d831ec58a5e11ab5e5aaa4f
Jacek programmer <jacek301@gmail.com>
2024-05-15 21:51:10 +0200

    Simple multiplication kata start

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,17 +1,23 @@
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
-
 import org.junit.runners.JUnit4;
 
 public class SolutionTest {
-
     @Test
-    public void testSomething() {
-        assertEquals(Solution.check(new Object[]{66, 101}, 66), true);
-        assertEquals(Solution.check(new Object[]{80, 117, 115, 104, 45, 85, 112, 115}, 45), true);
-        assertEquals(Solution.check(new Object[]{'t', 'e', 's', 't'}, 'e'), true);
-        assertEquals(Solution.check(new Object[]{"what", "a", "great", "kata"}, "kat"), false);
+    public void test1() {
+        System.out.println("Checking for Odd numbers.");
+        assertEquals(9, Sid.simpleMultiplication(1));
+        assertEquals(27, Sid.simpleMultiplication(3));
+        assertEquals(189, Sid.simpleMultiplication(21));
+        assertEquals(207, Sid.simpleMultiplication(23));
     }
 
+    @Test
+    public void test2() {
+        System.out.println("Checking for Even numbers.");
+        assertEquals(16, Sid.simpleMultiplication(2));
+        assertEquals(32, Sid.simpleMultiplication(4));
+        assertEquals(176, Sid.simpleMultiplication(22));
+        assertEquals(208, Sid.simpleMultiplication(26));
+    }
 }
