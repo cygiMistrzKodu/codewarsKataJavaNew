58fdaddfa4dbf9f08ab6fc385f2a6f0cd5c4f6ea
Jacek programmer <jacek301@gmail.com>
2024-05-16 01:55:28 +0200

    If you can't sleep, just count sheep!! kata solution

--- src/main/java/Sid.java
+++ src/main/java/Sid.java
@@ -1,6 +1,12 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
 class Kata {
     public static String countingSheep(int num) {
-        //Add your code here
-        return "";
+
+     List<String> sheepList =  IntStream.range(1, num+1).mapToObj(String::valueOf)
+                .map(nextSheepNumber -> nextSheepNumber + " sheep...").toList();
+
+        return String.join("", sheepList);
     }
 }
