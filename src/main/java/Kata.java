8c0e881b02be1da7d2e0277c7383982bc2008081
Jacek programmer <jacek301@gmail.com>
2024-05-16 20:27:21 +0200

    Array.diff kata Solution

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Kata {
 
     public static int[] arrayDiff(int[] a, int[] b) {
-        // Your code here
-        return a;
+
+        List<Integer> listA = Arrays.stream(a).boxed().toList();
+        List<Integer> listB = Arrays.stream(b).boxed().toList();
+
+        List<Integer> difference = new ArrayList<>(listA);
+        for (Integer numberB : listB) {
+
+            for (Integer numberA : listA) {
+                if (numberA.equals(numberB)) {
+                    difference.remove(numberB);
+                }
+            }
+        }
+
+        return difference.stream().mapToInt(i -> i).toArray();
     }
 
 }
