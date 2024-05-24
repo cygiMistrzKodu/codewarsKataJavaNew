c484f77b5d5751303e084ccde0277cf282e26ad0
Jacek programmer <jacek301@gmail.com>
2024-05-24 23:35:38 +0200

    Build Tower kata solution

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,8 +1,8 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
-// TODO: Replace examples and use TDD development by writing your own tests
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 
 public class SolutionTest {
     @Test
@@ -10,5 +10,20 @@ public class SolutionTest {
         assertEquals(String.join(",", "*"), String.join(",", Kata.towerBuilder(1)));
         assertEquals(String.join(",", " * ", "***"), String.join(",", Kata.towerBuilder(2)));
         assertEquals(String.join(",", "  *  ", " *** ", "*****"), String.join(",", Kata.towerBuilder(3)));
+    }
+
+
+    @Test
+    public void spaceCalculatorTests() {
+
+        assertArrayEquals(new Integer[]{0} ,Kata.calculateSpace(1).toArray());
+        assertArrayEquals(new Integer[]{1,0} ,Kata.calculateSpace(2).toArray());
+        assertArrayEquals(new Integer[]{2,1,0} ,Kata.calculateSpace(3).toArray());
+        assertArrayEquals(new Integer[]{3,2,1,0} ,Kata.calculateSpace(4).toArray());
+        assertArrayEquals(new Integer[]{4,3,2,1,0} ,Kata.calculateSpace(5).toArray());
+        assertArrayEquals(new Integer[]{5,4,3,2,1,0} ,Kata.calculateSpace(6).toArray());
+        assertArrayEquals(new Integer[]{6,5,4,3,2,1,0} ,Kata.calculateSpace(7).toArray());
+
+
     }
 }
