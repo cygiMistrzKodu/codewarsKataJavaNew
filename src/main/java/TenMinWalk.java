a2676ec19d4542daaba69d312ca440c78c9fd6f9
Jacek programmer <jacek301@gmail.com>
2024-05-25 20:50:18 +0200

    Take a Ten Minutes Walk kata Solution

--- src/main/java/TenMinWalk.java
+++ src/main/java/TenMinWalk.java
@@ -1,6 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TenMinWalk {
     public static boolean isValid(char[] walk) {
-        // Insert brilliant code here
-        return true;
+
+        Map<Character, Integer> directionAndMovingVectorNorthSouth = new HashMap<>();
+        directionAndMovingVectorNorthSouth.put('n', 1);
+        directionAndMovingVectorNorthSouth.put('s', -1);
+
+        Map<Character, Integer> directionAndMovingVectorWestEast = new HashMap<>();
+        directionAndMovingVectorWestEast.put('e', 1);
+        directionAndMovingVectorWestEast.put('w', -1);
+
+        boolean isWalkTake10Minutes = walk.length == 10;
+
+        if (isWalkTake10Minutes) {
+
+            int displacementNorthSouth = 0;
+            int displacementWestEast = 0;
+
+            for (char directionLetter : walk) {
+                displacementNorthSouth += directionAndMovingVectorNorthSouth.getOrDefault(directionLetter, 0);
+                displacementWestEast += directionAndMovingVectorWestEast.getOrDefault(directionLetter, 0);
+            }
+            return (displacementNorthSouth == 0) && (displacementWestEast == 0);
+
+        }
+
+        return false;
     }
 }
