0a532bdb1906308cc2d1295f43502c62c2b56064
Jacek programmer <jacek301@gmail.com>
2024-05-26 19:00:14 +0200

    Find the odd int kata solution

--- src/main/java/FindOdd.java
+++ src/main/java/FindOdd.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FindOdd {
     public static int findIt(int[] a) {
-        return 0;
+
+        Map<Integer, Long> numberAndOccurrence = Arrays.stream(a).boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Optional<Map.Entry<Integer, Long>> numberOddNumbersOfTime =
+                numberAndOccurrence.entrySet().stream()
+                        .filter(number -> number.getValue() % 2 != 0).findFirst();
+
+        return numberOddNumbersOfTime.get().getKey();
     }
 }
